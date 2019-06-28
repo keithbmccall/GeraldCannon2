0525c4705389727974f3cd6338925e322d6e3222
keithmccall <keithcodes@gmail.com>
2019-06-28 12:08:32 -0400

    works page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,17 @@
+function debounce(func, wait, immediate) {
+    let timeout;
+    return function () {
+        let context = this, args = arguments;
+        const later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        let callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 (function ($, window, document) {
     $(function () {
         const $win = $(window);
@@ -19,23 +33,26 @@
                 const width = tw > mw ? tw : mw
                 $scrollContainer.width(width);
                 return width
-            }
+            };
             const options = {
                 height: setScrollContainerWidth(), // optional: define the height the user can scroll, otherwise the overall length will be taken as scrollable height
                 onScroll: function (percent) {   //optional: callback function that will be called when the user scrolls down, useful for animating other things on the page
                     console.log(percent);
                 }
             }
-            const elem = $.jInvertScroll([`.${wrapper} .invert_scroll`], options);
+            $.jInvertScroll([`.${wrapper} .invert_scroll`], options);
             $(`.${wrapper}`).addClass('ready')
-            $win.resize(() => {
-                if ($(window).width() <= 768) {
-                    elem.destroy();
-                } else {
-                    elem.reinitialize();
-                }
-            });
         }
+        const reInitInvertScroll = wrapper => {
+            $win.scrollTop(0);
+            $(`.${wrapper}`).removeClass('ready')
+            initInvertScroll("works");
+        }
+        const invertScroll = {
+            init: initInvertScroll,
+            reInit: reInitInvertScroll
+        }
+
         // end invert scroll plug
         // nav launcher
         const toggleNav = () => {
@@ -44,20 +61,19 @@
         // end nav launcher
         const attachHandlers = () => {
             if ($('.invert_scroll').length) {
+                const _reInitInvertScroll = debounce(() => invertScroll.reInit('works'), 250)
+                $win.on('resize', _reInitInvertScroll);
                 setTimeout(() => {
-                    initInvertScroll("works");
+                    invertScroll.init("works");
                 }, 1000)
             }
 
             if ($('.menu-icon, .close-icon').length) {
-                const $navLaunchers = $('.menu-icon, .close-icon');
-                $navLaunchers.on('click', toggleNav)
+                $('.menu-icon, .close-icon').on('click', toggleNav)
             }
+
         }
         attachHandlers();
-
-
-
 
 
     });
