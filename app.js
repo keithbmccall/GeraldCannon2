53162569026d52f9bfab52113a0dabd155a29776
keithmccall <keithcodes@gmail.com>
2024-05-10 12:13:30 -0700

    turning off ping

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const http = require("http");
-setInterval(function() {
-    http.get("http://geraldxcannon.com");
-}, 300000);
+// setInterval(function() {
+//     http.get("http://geraldxcannon.com");
+// }, 300000);
 //
 //
 const app = express();
